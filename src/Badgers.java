2feffc69536529739a480b8cba9bdae50d9d33de
Carnecchiad <league@WorthyMini-5.attlocal.net>
2018-04-08 15:03:51 -0700

    My Ages, Badgers

---
+++ src/Badgers.java
@@ -0,0 +1,16 @@
+
+public class Badgers {
+	public static void main(String[] args) {
+		for (int j = 0; j < 3; j++) {
+		for (int t = 0; t < 12; t++) {
+			System.out.println("Badger");
+		} 
+		for (int i = 0; i < 2; i++) {
+			System.out.println("mushroom");
+		}
+	}
+		for (int s = 0; s < 2; s++) {
+			System.out.println("snake");
+	}
+}
+}
